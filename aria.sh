48e5dc7901ce03e1b589eee8cf6bd22aeb2b417e
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2021-03-23 11:48:06 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 export MAX_DOWNLOAD_SPEED=0
-tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=7
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/yuno74/tracker/main/tracker.txt | awk '$1' | tr '\n' ',')
+export MAX_CONCURRENT_DOWNLOADS=6
 
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
