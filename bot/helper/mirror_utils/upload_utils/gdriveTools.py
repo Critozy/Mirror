23e6e43e75b15106f6ea3cd25b79dd185adc2c4b
JanganCrut <46706073+JanganCrut@users.noreply.github.com>
2021-08-24 01:11:54 +0800

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -98,7 +98,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded using Minmi Mirror Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -478,9 +478,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'Minmi Mirror Bot Search',
+                                 author_name='Minmi Mirror Bot',
+                                 author_url='https://github.com/breakdowns/Minmi-mirrorbot',
                                  html_content=content)
         return
 
@@ -551,9 +551,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'Minmi Mirror Bot Search',
+                                                        author_name='Minmi Mirror Bot',
+                                                        author_url='https://github.com/breakdowns/Minmi-mirrorbot',
                                                         html_content=content
                                                         )['path'])
 
